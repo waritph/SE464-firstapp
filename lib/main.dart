bdebcdfd040531c584ea4763c1e22c84b612b967
waritph <warit_phanta@cmu.ac.thm>
2025-12-02 09:45:15 +0700

    SE464-firstapp

---
+++ lib/main.dart
@@ -0,0 +1,160 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'WARIT SHOP',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const MyHomePage(title: 'WARIT SHOP'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  final TextEditingController _priceController = TextEditingController();
+  final TextEditingController _amountController = TextEditingController();
+  final TextEditingController _receiveController = TextEditingController();
+
+  double _total = 0.0;
+  double _change = 0.0;
+
+  @override
+  void dispose() {
+    _priceController.dispose();
+    _amountController.dispose();
+    _receiveController.dispose();
+    super.dispose();
+  }
+
+  void _calculateTotal() {
+    final priceValue = double.tryParse(_priceController.text);
+    final amountValue = double.tryParse(_amountController.text);
+    if (priceValue == null || amountValue == null) {
+      return;
+    }
+
+    setState(() {
+      _total = priceValue * amountValue;
+    });
+  }
+
+  void _calculateChange() {
+    final receiveValue = double.tryParse(_receiveController.text);
+    if (receiveValue == null) {
+      return;
+    }
+
+    setState(() {
+      _change = receiveValue - _total;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        title: Text(widget.title),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(20.0),
+        child: Column(
+          children: [
+            _priceTextField(),
+            const SizedBox(height: 20),
+            _amountTextField(),
+            const SizedBox(height: 20),
+            _calculateButton(),
+            const SizedBox(height: 20),
+            _showTotalText(),
+            const SizedBox(height: 20),
+            _receiveMoneyTextField(),
+            const SizedBox(height: 20),
+            _changeCalculateButton(),
+            const SizedBox(height: 20),
+            _showChangeText(),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget _priceTextField() {
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: TextField(
+        controller: _priceController,
+        decoration: const InputDecoration(
+          border: OutlineInputBorder(),
+          labelText: 'Price per item',
+        ),
+        keyboardType:
+            const TextInputType.numberWithOptions(decimal: true, signed: false),
+      ),
+    );
+  }
+
+  Widget _amountTextField() {
+    return TextField(
+      controller: _amountController,
+      decoration: const InputDecoration(
+        border: OutlineInputBorder(),
+        labelText: 'Amount of items',
+      ),
+      keyboardType: TextInputType.number,
+    );
+  }
+
+  Widget _calculateButton() {
+    return ElevatedButton(
+      onPressed: _calculateTotal,
+      child: const Text('Calculate Total'),
+    );
+  }
+
+  Widget _showTotalText() {
+    return Text('Total: $_total Bath');
+  }
+
+  Widget _receiveMoneyTextField() {
+    return TextField(
+      controller: _receiveController,
+      decoration: const InputDecoration(
+        border: OutlineInputBorder(),
+        labelText: 'Receive money',
+      ),
+      keyboardType:
+          const TextInputType.numberWithOptions(decimal: true, signed: false),
+    );
+  }
+
+  Widget _changeCalculateButton() {
+    return ElevatedButton(
+      onPressed: _calculateChange,
+      child: const Text('Calculate Change'),
+    );
+  }
+
+  Widget _showChangeText() {
+    return Text('Change: $_change Bath');
+  }
+}
