9fba91f8e5c9092f146ab779307ddc8d911b5d82
waritph <warit_phanta@cmu.ac.thm>
2025-12-26 10:57:13 +0700

    finish lab 7.2 , 7.3

--- lib/pages/detail.dart
+++ lib/pages/detail.dart
@@ -1,24 +1,46 @@
 import 'package:flutter/material.dart';
 
 class DetailPage extends StatefulWidget {
-  const DetailPage({super.key});
+  //const DetailPage({super.key});
+  final v1, v2, v3, v4;
+  DetailPage(this.v1,this.v2,this.v3,this.v4);
 
+  /*@override
+  State<DetailPage> createState() => _DetailPageState();*/
   @override
-  State<DetailPage> createState() => _DetailPageState();
+  _DetailPageState createState() => _DetailPageState();
 }
 
 class _DetailPageState extends State<DetailPage> {
+  var _v1, _v2, _v3, _v4;
+  @override
+  void initState() {
+    super.initState();
+    _v1 = widget.v1;
+    _v2 = widget.v2;
+    _v3 = widget.v3;
+    _v4 = widget.v4;
+  }
+
+
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: const Text("Details"),
       ), // ❗ ต้องมี comma
-      body: ListView(
-        children: const [
-          Text("Details"),
-        ],
+      body: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: ListView(
+          children: [
+            Text(_v1),
+            Text(_v2),
+            Image.network(_v3),
+            Text(_v4),
+          ],
       ),
+      )
     ); // ❗ ต้องมี ;
-  }
+  } 
 }
--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -1,3 +1,4 @@
+import 'dart:convert';
 import 'package:flutter/material.dart';
 import 'package:firstapp/pages/detail.dart';
 
@@ -17,30 +18,44 @@ class _AboutPageState extends State<AboutPage> {
       ), // ❗ ต้องมี comma
       body: Padding(
         padding: const EdgeInsets.all(20),
-        child: ListView(
-          children: [
-            myBox("About 001", "1111111111", "https://media4.giphy.com/media/v1.Y2lkPTc5MGI3NjExaGIwZThsa3lpZ2YwcGQ3NjRsNm5pZ2c1c2NxeXA3YWMwamZ1dWgwZSZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/lGrxvmD7YazFS/giphy.gif"),
-            const SizedBox(height: 20),
-            myBox("About 002", "2222222222", "https://media4.giphy.com/media/v1.Y2lkPTc5MGI3NjExMnBzcTByMGlseXFicjlqaHA4dWJsa3g0ZGNqczhmM2MxNm94YWF4MSZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/AAsj7jdrHjtp6/giphy.gif"),
-            const SizedBox(height: 20),
-            myBox("About 003", "3333333333", "https://media3.giphy.com/media/v1.Y2lkPTc5MGI3NjExZGo1eXp5NTdlajV1Nmh3ZTg1bzF0eXQ1dG9od3RiNzlrbDRzb3BmNSZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/6Uqr0IDWkzhBu/giphy.gif"),
-            const SizedBox(height: 20),
-          ],
+        child: FutureBuilder(
+          builder: (context, snapshot) {
+            var data = json.decode(snapshot.data.toString());
+            return ListView.builder(
+              itemBuilder: (BuildContext context, int index) {
+                return myBox(
+                  data[index]['title'],
+                  data[index]['subtitle'],
+                  data[index]['image_url'],
+                  data[index]['datail'],
+                );
+              },
+              itemCount: data.length,
+            );
+          },
+          future: DefaultAssetBundle.of(context).loadString('assets/data.json'),
         ),
       ),
     );
   }
 
   /// ✅ Widget function
-  Widget myBox(String title, String subtitle , String img_url) {
+  Widget myBox(String title, String subtitle, String image_url, String detail) {
+    var v1, v2, v3, v4;
+    v1 = title;
+    v2 = subtitle;
+    v3 = image_url;
+    v4 = detail;
+
     return Container(
+      margin: EdgeInsets.only(top: 20),
       padding: const EdgeInsets.all(20),
       height: 150, // ❗ ต้องมี comma
       decoration: BoxDecoration(
         color: Colors.pinkAccent,
         borderRadius: BorderRadius.circular(12),
-        image:  DecorationImage(
-          image: NetworkImage(img_url),
+        image: DecorationImage(
+          image: NetworkImage(image_url),
           fit: BoxFit.cover,
           colorFilter: ColorFilter.mode(
             Colors.black54,
@@ -69,18 +84,16 @@ class _AboutPageState extends State<AboutPage> {
               color: Colors.white,
             ),
           ),
-          SizedBox(height:2,),
+          SizedBox(height: 2),
           TextButton(
             onPressed: () {
               print("next page >>");
               Navigator.push(
                 context,
-                MaterialPageRoute(builder: (context) => DetailPage()),
-
+                MaterialPageRoute(builder: (context) => DetailPage(v1, v2, v3, v4)),
               );
-
             },
-            child: Text("read more", style: TextStyle(color: Colors.white))
+            child: Text("read more", style: TextStyle(color: Colors.white)),
           ),
         ],
       ),
